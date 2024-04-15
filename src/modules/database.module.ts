b66ffa8b355d1592770b9248ce2352db21ee822a
Nam077 <nam077.me@gmail.com>
2024-04-15 22:05:42 +0700

    feat: generate user module

--- src/modules/database.module.ts
+++ src/modules/database.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MainConfigServiceService } from './database/service/main-config-service.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       name: 'default',
       useClass: MainConfigServiceService,
     }),
+    UserModule,
   ],
   providers: [MainConfigServiceService],
 })
