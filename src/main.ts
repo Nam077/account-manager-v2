19219fe46c09e985c61262f8131c4d39ea0d4eb4
Nam077 <nam077.me@gmail.com>
2024-04-15 21:58:43 +0700

    feat: add swagger and classvalidtor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {});
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Account management API')
+    .setDescription('Project for account management')
+    .setVersion('1.0')
+    .addTag('account-management')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
-bootstrap();
+(async () => {
+  await bootstrap();
+})();
