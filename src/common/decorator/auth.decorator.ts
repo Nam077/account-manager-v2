f4a83528a6be39ee7560d77a6ec6a6602c4f1a5a
Nam077 <nam077.me@gmail.com>
2024-04-24 12:25:48 +0700

    docs: update doc

--- src/common/decorator/auth.decorator.ts
+++ src/common/decorator/auth.decorator.ts
@@ -6,8 +6,18 @@ import { User } from '../../modules/user/entities/user.entity';
 export const IS_PUBLIC_KEY = 'isPublic';
 export const ROLE_KEY = 'role';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+/**
+ * Decorator function that sets the metadata for the role of a user.
+ * @param role - The role of the user.
+ */
 export const Role = (role: string) => SetMetadata(ROLE_KEY, role);
 
+/**
+ * Custom decorator to get the current user.
+ * @param data - Optional property of the User object to retrieve.
+ * @param context - The execution context.
+ * @returns A Promise that resolves to the User object or any value.
+ */
 export const GetCurrentUser = createParamDecorator(
     async (data: keyof User | undefined, context: ExecutionContext): Promise<User | any> => {
         const user$: Observable<User> = context.switchToHttp().getRequest().user;
